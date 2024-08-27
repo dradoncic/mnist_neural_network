4f4d68569e5bd23fca7cd354ceaae2e9ffcc525c
dradoncic <deenr214@gmail.com>
2024-08-27 18:43:42 -0400

    mnist_training

--- src/mnist_training.py
+++ src/mnist_training.py
@@ -4,7 +4,13 @@ import numpy as np
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0], -1).astype(np.float32) / 255.0
-X_test = X_test.reshape(X_test.shape[0], -1).astype(np.float32) / 255.0
+keys = np.array(range(X_train.shape[0]))
+np.random.shuffle(keys)
+X_train = X_train[keys]
+y_train = y_train[keys]
+
+
+X_train = (X_train.reshape(X_train.shape[0], -1).astype(np.float32) - 127.5) / 127.5
+X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 
 print(X_train.shape[1])
