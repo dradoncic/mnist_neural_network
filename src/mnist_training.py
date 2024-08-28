678f9258d4159655e84c76801cbe0cc0b62f246d
dradoncic <deenr214@gmail.com>
2024-08-27 20:07:55 -0400

    final commit

--- src/mnist_training.py
+++ src/mnist_training.py
@@ -13,4 +13,15 @@ y_train = y_train[keys]
 X_train = (X_train.reshape(X_train.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 
-print(X_train.shape[1])
+model = Model()
+
+model.add(HiddenLayer(X_train.shape[1], 128))
+model.add(ReLU())
+model.add(HiddenLayer(128, 128))
+model.add(ReLU())
+model.add(HiddenLayer(128, 10))
+model.add(SoftMax())
+
+model.wrap()
+
+model.train(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=128, print_every=100)
--- src/neural_network.py
+++ src/neural_network.py
@@ -96,7 +96,7 @@ class SoftMaxCrossEntropyLossBackward:
 # SGD Optimizer: Implements Stochastic Gradient Descent
 class StochasticGradientDescent:
 
-    def __init__(self, learning_rate=1.0, decay=0.0, momentum=0.0):
+    def __init__(self, learning_rate=0.1, decay=0.0, momentum=0.0):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
@@ -226,7 +226,7 @@ class Model:
     def __init__(self):
         self.layers = []
         self.loss = CrossEntropy()
-        self.optimizer = StochasticGradientDescent()
+        self.optimizer = StochasticGradientDescent(decay=1e-3)
         self.accuracy = CategoricalAccuracy()
         self.softmax_classifier_output = SoftMaxCrossEntropyLossBackward()
 
